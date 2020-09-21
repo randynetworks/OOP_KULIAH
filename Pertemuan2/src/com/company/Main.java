3d61a139e836846293fa15b183e097be4758b9de
randynetworks <randynetworks@gmail.com>
2020-09-21 09:09:56 +0700

    pertemuan 2 OOP membuat fungsi

---
+++ Pertemuan2/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Fugsi : f(x) = x^2 + 2x + 1");
+
+        for (int i = 1; i <= 10; i++) {
+            fungsi(i);
+        }
+    }
+
+    static void fungsi(Integer x) {
+        int perhitungan = (x * x) + (2 * x) + 1;
+        System.out.println("f("+x+") = " + x + "^2 " + "+ 2(" + x + ") + 1 = " + perhitungan);
+    }
+}
+
+
